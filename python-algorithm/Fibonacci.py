d8b0dd1e586519de7fc5f54d5d6b5bc37f9633e0
AirRain <31362594+airrain@users.noreply.github.com>
2019-01-14 20:13:30 +0800

    add algorithm
    add algorithm

---
+++ python-algorithm/Fibonacci.py
@@ -0,0 +1,32 @@
+"""问题1: 
+	给定一个整数N，返回斐波那契数列的第N项？
+"""
+
+def fibonacci_1(n):
+	if n < 0:
+		raise ValueError("The n is illegall!")
+
+	if n < 3:
+		return 1
+
+	return fibonacci_1(n - 1) + fibonacci_1(n - 2)
+
+def fibonacci_2(n):
+	if n < 0:
+	raise ValueError("The n is Illegall!")
+
+	if n < 3:
+	return 1
+
+    a,b = 1,1
+    for _ in range(3,n+1):
+    	tmp = a + b
+    	a = b
+    	b = tmp
+    return tmp
+
+def fibonacci_3(n):
+	pass
+
+
+
