a91d8d87b0e5a28fd7e9a971662c7e89e0c01c52
AirRain <31362594+airrain@users.noreply.github.com>
2019-01-17 11:57:14 +0800

    add question
    add question

--- LeetCode题解/AddTwoNumbers.py
+++ LeetCode题解/AddTwoNumbers.py
@@ -22,8 +22,22 @@ class ListNode(object):
 
 class Solution(object):
     def addTwoNumbers(self,l1,l2):
-
-
+        result = ListNode(0)
+        cur = result
+        while l1 or l2:
+            cur.val += self.addTwoNodes(l1,l2)
+            if cur.val >= 10:
+                cur.val -= 10
+                cur.next = ListNode(1)
+            else:
+                if l1 and l1.next or l2 and l2.next:
+                    cur.next = ListNode(0)
+                cur = cur.next
+                if l1:
+                    l1 = l1.next
+                if l2:
+                    l2 = l2.next
+                return result
 
     def addTwoNodes(self,n1,n2):
         if not n1 and n2:
@@ -33,3 +47,7 @@ class Solution(object):
         if not n2:
             return n1.val
         return n1.val + n2.val
+if __name__ =='__main__':
+    list = ListNode(9)
+    list.next = ListNode(8)
+    print(Solution().addTwoNumbers(list, ListNode(1)).myPrint())
