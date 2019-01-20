134a05db7f16b41afb218fb509884d88f525b9f5
AirRain <31362594+airrain@users.noreply.github.com>
2019-01-20 10:39:07 +0800

    add question
    add question

--- LeetCode题解/IntegerToRoman.py
+++ LeetCode题解/IntegerToRoman.py
@@ -20,5 +20,15 @@
 """
 class Solution(object):
     def intToRoman(self,num):
+        nums = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        strings = ["M","CM","D","C","XC","L","XL","IX","V","IV","I"]
+        result = ''
+        for i in range(len(nums)):
+            while num >= nums[i]:
+                num -= nums[i]
+                result += strings[i]
+            return result
 
-	
+if __name__ =='__main__':
+   Solution().intToRoman(12)
+
