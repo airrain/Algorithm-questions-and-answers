f584a515008774262e320cfcee1a016d01553873
AirRain <31362594+airrain@users.noreply.github.com>
2019-01-21 21:01:28 +0800

    add question
    add question

---
+++ LeetCode题解/Permutations.py
@@ -0,0 +1,24 @@
+"""
+输出一个没有重复数字的数组的全排列。
+
+注意点：
+
+    不用考虑重复数字的情况
+
+例子：
+
+输入: nums = [1, 2, 3] 输出: [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]
+解题思路
+
+这道题比较单间，采用递归把数组中的数字依次加入当前数组current来进行排列组合。
+"""
+class Solution(object):
+    def permute(self,nums):
+        result = []
+        self.get_permute([],nums,result)
+        return result
+
+    def get_permute(self,current,num,result):
+        if not num:
+
+
