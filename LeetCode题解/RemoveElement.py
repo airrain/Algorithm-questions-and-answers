c1e91bd6e9befc21f1058b4b2ec2db97b1e0c9f5
AirRain <31362594+airrain@users.noreply.github.com>
2019-01-21 20:13:12 +0800

    add question
    add question

--- LeetCode题解/RemoveElement.py
+++ LeetCode题解/RemoveElement.py
@@ -14,12 +14,19 @@
 左右两个指针向中间靠拢，左指针找到一个等于val的值，右指针找到第一个不等于val的值，把右指针指向的值赋值给左指针。继续向中间靠拢。
 """
 class Solution(object):
-    def removeElement(self,nums,x):
+    def removeElement(self,nums,val):
         left = 0
-        right = len(sums) - 1
+        right = len(nums) - 1
         while left <= right:
-
-
+            while left <= right and left != val:
+                left += 1
+            while left <= right and right == val:
+                right -= 1
+            if left < right:
+                nums[left] = nums[right]
+                left += 1
+                right -=1
+            return right + 1
 
 if __name__ == "__main__":
-    Solution().removeElement([1,3,4,2,6])
+    print(Solution().removeElement([1,3,4,2,6,3,5,2],2))
