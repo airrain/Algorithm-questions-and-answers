ac427f855b9f60ca9245adead9b42ee5556179f9
AirRain <31362594+airrain@users.noreply.github.com>
2019-02-01 21:05:47 +0800

    add question
    add question

--- LeetCode题解/Triangle.py
+++ LeetCode题解/Triangle.py
@@ -34,7 +34,20 @@ class Solution(object):
 	def minimumTotal(self,triangle):
 		n = len(triangle)
 		dp = triangle[-1]
-		for i in range()
+		for i in range(n - 2,-1,-1):
+			for j in range(i + 1):
+				dp[j] = triangle[i][j] + min(dp[j],dp[j + 1])
+		return dp[0]
+
+if __name__ == "main":
+	Solution().minimumTotal([
+		[2],
+		[3],[4],
+		[6],[5],[7],
+		[4],[1],[8],[3]
+		])
+
+
 
 
 
