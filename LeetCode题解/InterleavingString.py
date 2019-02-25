9216bd3a5131985e7a513fab107d00f0f7444add
AirRain <31362594+airrain@users.noreply.github.com>
2019-02-25 19:33:54 +0800

    add question
    add question

--- LeetCode题解/InterleavingString.py
+++ LeetCode题解/InterleavingString.py
@@ -26,8 +26,19 @@ class Solution(object):
         m = len(s1)
         n = len(s2)
         l = len(s3)
-        if n
+        if m + n != l:
+            return False
+        dp = [True for __ in range(m + 1)]
+        for i in range(m):
+            dp[i + 1] = dp[i] and s1[i] == s3[i]
+        for j in range(n):
+            dp[0] = dp[0] and s2[j] == s3[j]
+            for i in range(m):
+                dp[i + 1] = (dp[i] and s1[i] == s3[i + j + 1]) or (dp[i +1] and s2[j] == s3[i + j + 1])
+            return dp[m]
 
+if __name__ == "__main__":
+    print(Solution().isInterleave("aabcc","abbca","aadbbcbcac"))
 
 
 
