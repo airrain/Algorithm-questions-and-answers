6601a21d1e3f2d528b2900d43df9c10cd7e65e59
AirRain <31362594+airrain@users.noreply.github.com>
2019-02-25 20:32:50 +0800

    add question
    add question

--- LeetCode题解/DistinctSubsequences.py
+++ LeetCode题解/DistinctSubsequences.py
@@ -16,4 +16,15 @@
 """
 class Solution(object):
     def numDistinct(self,s,t):
-        
+        m = len(s)
+        n = len(t)
+        dp = [0 for __ in range(n + 1)]
+        dp[0] = 1
+        for i in range(m):
+            for j in range(n - 1,-1,-1):
+                if t[j] == s[i]:
+                    dp[j + 1] += dp[j]
+        return dp[-1]
+
+if __name__ == "__main__":
+    print(Solution().numDistinct("rabbbit","rabbit"))
