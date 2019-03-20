7e7f4fb1138b358d4fe21842be27b92d50d3a273
AirRain <31362594+airrain@users.noreply.github.com>
2019-03-20 10:29:04 +0800

    add question
    add question

--- LeetCode题解/PartitionList.py
+++ LeetCode题解/PartitionList.py
@@ -47,4 +47,16 @@ class Solution(object):
 
 if __name__ == "__main__":
     n1 = ListNode(1)
+    n2 = ListNode(4)
+    n3 = ListNode(3)
+    n4 = ListNode(2)
+    n5 = ListNode(5)
+    n6 = ListNode(2)
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+    n5.next = n6
+    r = Solution().partition(n1,3)
+    r.to_list()
 
