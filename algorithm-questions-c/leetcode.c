f6dda7f301cc8f842645bf8ca57fb4f5d5ceef72
AirRain <31362594+airrain@users.noreply.github.com>
2019-06-04 09:25:50 +0800

    Update leetcode.c

--- algorithm-questions-c/leetcode.c
+++ algorithm-questions-c/leetcode.c
@@ -1,12 +1,12 @@
 //字符串前面的若干字符移动到字符串后面解法一
- void fun LeftShiftOne(char *s,int n){
+ void LeftShiftOne(char *s,int n){
      char t = s[0];
      for(int i = 1;i < n;i++){
          s[i - 1] = s[i];
      }
      s[n -1] = t;
  }
- void fun LeftRotateString(char *s,int n,int m){
+ void LeftRotateString(char *s,int n,int m){
      while(m--){
          LeftShiftOne(s,n);
      }
