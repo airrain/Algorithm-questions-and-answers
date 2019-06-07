e04f9b4ba41887e69a874f7d537fe8ecc6e9030a
AirRain <31362594+airrain@users.noreply.github.com>
2019-06-07 11:51:19 +0800

    Update leetcode.cpp

--- algorithm-questions-cpp/leetcode.cpp
+++ algorithm-questions-cpp/leetcode.cpp
@@ -1,38 +1,46 @@
 //输入字符串，打印出其全排列
 
 //判断回文串解法一
-bool isPalindrom(const char *s,int n){
-    if(s == 0 || n < 1){
+bool isPalindrom(const char *s, int n)
+{
+    if (s == 0 || n < 1)
+    {
         return false;
     }
-    const char *front,*back;
-    while(front < back){
-        if(*front != *back){
+    const char *front, *back;
+    while (front < back)
+    {
+        if (*front != *back)
+        {
             return false;
         }
         ++front;
         --back;
     }
     return true;
-
 }
 //判断回文串解法一
 
 //跳台阶问题解法一
-long long Fibonacci(unsigned int n){
-    int result[3] = {0,1,2};
-    if(n < 2){
+long long Fibonacci(unsigned int n)
+{
+    int result[3] = {0, 1, 2};
+    if (n < 2)
+    {
         return result[n];
     }
-    return Fibonacci(n-1) + Fibonacci(n-2);
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 //跳台阶问题解法二
-int climbStairs(int n){
-    int dp[3] = {1,1};
-    if(n<2){
+int climbStairs(int n)
+{
+    int dp[3] = {1, 1};
+    if (n < 2)
+    {
         return 1;
-        }
-    for(int i=2;i<=n;i++){
+    }
+    for (int i = 2; i <= n; i++)
+    {
         dp[2] = dp[0] + dp[1];
         dp[0] = dp[1];
         dp[1] = dp[2];
@@ -42,3 +50,26 @@ int climbStairs(int n){
 
 //奇偶数排序
 
+//有序数组的查找
+int BinarySearch(int arr[], int n, int value)
+{
+    int left = arr[0];
+    int right = arr[n - 1];
+    while (left <= right)
+    {
+        int mid = left + (right - left) << 1;
+        if (value < arr[mid])
+        {
+            right = mid - 1;
+        }
+        else if (value > arr[mid])
+        {
+            left = mid + 1;
+        }
+        else
+            {
+                return mid;
+            }
+    }
+    return -1;
+}
