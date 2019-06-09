b3224da72d4c3810f46b7481ad2880fe199f7a9b
AirRain <31362594+airrain@users.noreply.github.com>
2019-06-09 21:01:28 +0800

    Update leetcode.py

--- algorithm-questions-python/leetcode.py
+++ algorithm-questions-python/leetcode.py
@@ -17,3 +17,15 @@ if __name__ == "__main__":
     str = "abcde"
     print("字符串"+str+"反转以后：")
     print(str.reverseStr(str))
+
+#统计字符串中连续的重复字符个数
+def getMaxDupChar(s,startIndex,currMaxNum,maxNum):
+    if startIndex == len(s):
+        return max(currMaxNum,maxNum)
+    if list(s)[startIndex] == list(s)[startIndex+1]:
+        return getMaxDupChar(s,startIndex+1,currMaxNum+1,maxNum)
+    else:
+        return getMaxDupChar(s,startIndex+1,1,max(currMaxNum,maxNum))
+
+if __name__ == "__main__":
+    print("连续重复字符个数最大值为"+str(getMaxDupChar("aaacccdd",0,1,1)))
