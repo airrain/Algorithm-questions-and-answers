74452aa62893810942bf7231addb2f04ffdf9952
AirRain <31362594+airrain@users.noreply.github.com>
2019-06-19 16:55:53 +0800

    Update leetcode.py

--- algorithm-questions-python/leetcode.py
+++ algorithm-questions-python/leetcode.py
@@ -278,9 +278,9 @@ if __name__ == "__main__":
 def countOne(n):
     count = 0
     while n > 0:
-        if n&1 == 1:
+        if (n&1) == 1:
             count += 1
-        n >> 1
+        n >>= 1
     return count
 
 if __name__ == "__main__":
