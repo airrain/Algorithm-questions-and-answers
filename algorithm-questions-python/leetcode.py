8759dd9dce0aee02395670736f7455c57cbee68a
AirRain <31362594+airrain@users.noreply.github.com>
2019-06-26 12:46:32 +0800

    Update leetcode.py

--- algorithm-questions-python/leetcode.py
+++ algorithm-questions-python/leetcode.py
@@ -341,18 +341,47 @@ class Solution(object):
 
 
 '''将一个链表中的倒数第n个元素从链表中去除'''
-class solution(object):
-    def __init__(self,val):
-        self.next = next
-        self.val = val
-    def myPrint(self,x):
+class ListNode(object):
+    def __init__(self,x):
+        self.next = None
+        self.val = x
+    def myPrint(self):
         print(self.val)
         if self.next:
-            myPrint(x.next)
+            self.next.myPrint()
+class Solution(object):
     def removeNThnumber(self,head,n):
         if not head:
             return head
+        permi = ListNode(-1)
+        permi.next = head
+        curr = permi
+        prev = permi
+        while prev and n >= 0:
+            prev = prev.next
+            n -= 1
+        while prev:
+            prev = prev.next
+            curr = curr.next
+        curr.next = curr.next.next
+        return permi.next
+
+if __name__ == "__main__":
+    n5 = ListNode(5) 
+    n4 = ListNode(4)
+    n3 = ListNode(3)
+    n2 = ListNode(2)
+    n1 = ListNode(1)
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+    result = Solution().removeNThnumber(n1,2)
+    result.myPrint()
+
         
+
+
         
 
 
