44b5c35bf56a4006e1c01f4b0feb5b8346493aef
AirRain <31362594+airrain@users.noreply.github.com>
2019-07-01 10:09:17 +0800

    Update leetcode.cpp

--- algorithm-questions-cpp/leetcode.cpp
+++ algorithm-questions-cpp/leetcode.cpp
@@ -120,3 +120,16 @@ void helan(int arr[],int n){
     }
 
 }
+
+//输入n个整数，找出其中最小的k个数。例如输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4
+
+//Partition函数
+int Partition(int data[],int length,int start,int end){
+    if(data == NULL || length <= 0 || start > end || end > length){
+        return
+    }
+    index = RandomInrange(start,end);
+
+
+}
+
